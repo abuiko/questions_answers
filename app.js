e16c386734d051533207567ce0b5d146903a7e49
Alena Buiko <abuiko.ma@gmail.com>
2021-03-01 15:58:22 -0500

    add/remove text when click btn

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const text = document.querySelectorAll('.question-text');
 const questionBtn = document.querySelectorAll('.question-btn');
+const text = document.querySelectorAll('.question-text');
 
 
 questionBtn.forEach(btn => btn.addEventListener('click', (e) => {
+    const question = e.target.parentElement.parentElement;
     const plus = e.target.firstElementChild;
     const minus = e.target.lastElementChild;
+
     plus.classList.toggle("show-text");
+
     if (plus.classList.contains('show-text')) {
         minus.classList.add("show-text");
+        question.lastElementChild.classList.add('show-text');
     } else {
         minus.classList.remove("show-text");
+        question.lastElementChild.classList.remove('show-text');
     }
 
 }));
